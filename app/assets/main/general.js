24e86f5a7c4cd2c01bc56f62cd71f9d3f09133eb
TienNM6 <tiennm6@Tien-Nguyen.local>
2015-12-16 11:28:38 +0700

    exclude some menu item when offline

--- app/assets/main/general.js
+++ app/assets/main/general.js
@@ -2243,7 +2243,7 @@ function displayOnline(isOffline){
 }
 
 function isOffline() {//check if application has internet connection
-	return true;
+	// return true;
 	if ((navigator.onLine || (navigator.connection && navigator.connection.type != Connection.NONE)) && offline == false) {
 		return false;
 	}
