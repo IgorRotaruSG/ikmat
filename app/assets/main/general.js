aa385279c79aed5fd0d8b7a7b28e5b9f284c450e
TienNM6 <tiennm6@Tien-Nguyen.local>
2015-12-28 10:32:17 +0700

    update 2.0.62

--- app/assets/main/general.js
+++ app/assets/main/general.js
@@ -1,9 +1,9 @@
 var settings = {
     //'apiDomain':        'http://haccpy11.bywmds.us/api/',
-    // 'apiDomain':        'http://ikmatapp.no/api/',
-    // 'apiPath':        'http://ikmatapp.no',
-    'apiDomain':        'http://10.16.43.33/api/',
-    'apiPath':        'http://10.16.43.33',
+    'apiDomain':        'http://ikmatapp.no/api/',
+    'apiPath':        'http://ikmatapp.no',
+    // 'apiDomain':        'http://10.16.43.33/api/',
+    // 'apiPath':        'http://10.16.43.33',
     'apiUploadPath':    'uploadPhotos',
 	'testImage' : 'apple-touch-icon.png',
 	'syncIntervals' : {// sync interval in ms (1000 ms = 1 second)
@@ -15,8 +15,8 @@ var settings = {
 	},
 	'requestTimeout' : 25000,
 	'excludeOffline': ["haccp.html", "flowchart.html"],
-	'version': "2.0.61",
-	'rebuild': "2.0.61"
+	'version': "2.0.62",
+	'rebuild': "2.0.62"
 };
 
 var performance = window.performance;
