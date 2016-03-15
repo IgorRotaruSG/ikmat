0b29ec6618b88cd388d0911dfcf9070c2a27a8f1
TienNM6 <tiennm6@Tien-Nguyen.local>
2016-03-15 18:31:55 +0700

    update haccp

--- app/assets/main/general.js
+++ app/assets/main/general.js
@@ -15,8 +15,8 @@ var settings = {
 	},
 	'requestTimeout' : 25000,
 	'excludeOffline' : ["haccp.html", "flowchart.html"],
-	'version' : "2.0.81",
-	'rebuild' : "2.0.81"
+	'version' : "2.0.82",
+	'rebuild' : "2.0.82"
 };
 
 var performance = window.performance;
