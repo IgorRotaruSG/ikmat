3af0492706274e7083b6bb114b875cc820111036
TienNM6 <tiennm6@Tien-Nguyen.local>
2016-05-20 17:29:07 +0700

    refactoring source code

--- build.sh
+++ build.sh
@@ -1,51 +1,25 @@
 #!/bin/sh
 SOURCE_PATH=`pwd`
-BACKEND_PATH='/Users/tiennm6/Documents/AUTOMAGI/ik-mat-backend'
 MOBILE_PATH='www'
-FSO_PATH='/usr/share/nginx/html/ikmat'
+PROJECT_NAME='IK-Mat'
 
-LIVE_ADDRESS='148.251.193.72'
-FSO_ADDRESS='10.16.43.33'
+# Install tools
+npm install -g grunt-cli@1.2.0  cordova@6.1.1
+#Added signing code
+security -v import "$SOURCE_PATH/certs/ios/apple.cer" -k "$HOME/Library/Keychains/login.keychain" -T /usr/bin/codesign
+security -v import "$SOURCE_PATH/certs/ios/dist.cer" -k "$HOME/Library/Keychains/login.keychain" -T /usr/bin/codesign
+security -v import "$SOURCE_PATH/certs/ios/dist.p12" -k "$HOME/Library/Keychains/login.keychain" -P Automagi2015 -T /usr/bin/codesign
 
-BACKEND_BRANCH='develop'
-SOURCE_BRANCH='scaffold'
-
-git pull origin $SOURCE_BRANCH
-REVISION=`git rev-parse origin/$SOURCE_BRANCH`
-if [ $1 ]
-then
-BACKEND_PATH=$1
-fi
-
-cd $BACKEND_PATH
-git checkout $BACKEND_BRANCH
-git pull
-rm -rf $BACKEND_PATH/web/app/*
-cd $SOURCE_PATH
-grunt build --target=$BACKEND_PATH/web/app --force&
-#grunt buildphone --target=$MOBILE_PATH&
-wait
-cd $BACKEND_PATH
-git add web/app
-git commit -am "build revision $REVISION"
-git push origin $BACKEND_BRANCH
+uuid=`grep UUID -A1 -a "$SOURCE_PATH/certs/ios/IKMATDistribution.mobileprovision" | grep -io "[-A-Z0-9]\{36\}"`
+cp "$SOURCE_PATH/certs/ios/IKMATDistribution.mobileprovision" "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
+rm -rf "$SOURCE_PATH/platforms"
+rm -rf "$SOURCE_PATH/plugins"
+#npm install
+#grunt build --force --target=$MOBILE_PATH
+phonegap platform add ios
+phonegap platform add android
+phonegap build ios --device --release
+phonegap build android --device --release
+cp "$SOURCE_PATH/platforms/android/build/outputs/apk/android-release.apk" "$SOURCE_PATH/builds/android/$PROJECT_NAME.apk"
+cp "$SOURCE_PATH/platforms/ios/build/device/$PROJECT_NAME.ipa" "$SOURCE_PATH/builds/ios/$PROJECT_NAME.ipa"
 echo "BUILD SUCCESSFUL"
-
-cd $SOURCE_PATH
-function fso_build
-{ # A somewhat more complex function.
-	ssh root@$FSO_ADDRESS "cd $FSO_PATH;git checkout $BACKEND_BRANCH; git checkout HEAD web/app; git pull; rm -rf $FSO_PATH/app/cache"
-	echo "DEPLOYE SUCCESSFUL TO FSO SERVER"
-}
-
-while true; do
-    read -p "Do you want to deploy source to live server? [Y/n]:" yn
-    case $yn in
-        [Yy]* ) fso_build; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-
-
--- platforms/ios/www/assets/main/language.js
+++
@@ -1,3 +0,0 @@
-var lang_string = {
-    'menu_registration': 'Registration1'
-}
