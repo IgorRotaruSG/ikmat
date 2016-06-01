eb2d8c24b74f93019ca00cc7c66d31e2314cba3c
TienNM6 <tiennm6@Tien-Nguyen.local>
2016-06-01 16:24:37 +0700

    update source build

--- build.sh
+++ build.sh
@@ -22,8 +22,8 @@ function ios_build
         security -v import "$SOURCE_PATH/certs/ios/dist.cer" -k "$HOME/Library/Keychains/login.keychain" -T /usr/bin/codesign
         security -v import "$SOURCE_PATH/certs/ios/dist.p12" -k "$HOME/Library/Keychains/login.keychain" -P Automagi2015 -T /usr/bin/codesign
 
-        uuid=`grep UUID -A1 -a "$SOURCE_PATH/certs/ios/IKMATDistribution.mobileprovision" | grep -io "[-A-Z0-9]\{36\}"`
-        cp "$SOURCE_PATH/certs/ios/IKMATDistribution.mobileprovision" "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
+        uuid=`grep UUID -A1 -a "$SOURCE_PATH/certs/ios/IKMAT_DIS.mobileprovision" | grep -io "[-A-Z0-9]\{36\}"`
+        cp "$SOURCE_PATH/certs/ios/IKMAT_DIS.mobileprovision" "$HOME/Library/MobileDevice/Provisioning Profiles/$uuid.mobileprovision"
         rm -rf "$SOURCE_PATH/platforms/ios"
         rm -rf "$SOURCE_PATH/plugins"
         phonegap platform add ios@3.9.2
